8f56b6cedecffab87c19a3039af31895aa6b19fe
Jeremy Downs <jeremy.l.downs@gmail.com>
2017-12-06 17:34:50 -0500

    Sanitize

--- wsgi.py
+++ wsgi.py
@@ -151,7 +151,7 @@ def app_query():
         sql_input = request.form.get("sql_input")
         sql_input = sql_input.replace(u"\u2018", "'").replace(u"\u2019", "'") # Sanitize unicode single quotes
         sql_input = sql_input.replace(u"\u201c", '"').replace(u"\u201d", '"') # Sanitize unicode double quotes
-        for uni in (u"\ufffd", u"\u25aa", u"\u2022", u"\uf0d8", u"\u2028"):
+        for uni in (u"\ufffd", u"\u25aa", u"\u2022", u"\uf0d8", u"\u2028", u"\u20ac"):
             sql_input = sql_input.replace(uni, '')
 
         to_results = request.form.get("to_results")
